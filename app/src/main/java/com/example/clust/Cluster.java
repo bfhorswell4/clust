0c2067b7567a1ae255926ebffe5dfe532c1296b9
bfhorswell4 <bfhorswell@gmail.com>
2021-04-23 18:34:10 +0100

    fixed issue where clustering not working as had not implemented center logic

--- app/src/main/java/com/example/clust/Cluster.java
+++ app/src/main/java/com/example/clust/Cluster.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
- * A class that represents a group of LatLng locations
+ * A class that represents a grouping of LatLng locations
  */
 public class Cluster {
     private ArrayList<LatLng> locations;
@@ -15,7 +15,7 @@ public class Cluster {
 
     /**
      * Initialises a Cluster
-     * @param location The initial single location that defines the cluster
+     * @param location An initial single location that defines the cluster
      */
     public Cluster(LatLng location){
         locations = new ArrayList<>();
@@ -24,21 +24,34 @@ public class Cluster {
     }
 
     /**
-     * This method calculates the current center of the cluster
-     * @return The current center coordinate of the cluster
+     * This method calculates the center of the cluster
+     * @return The center coordinates of the cluster
      */
     public LatLng getCenter(){
         if(this.locations.size()==1){
             return this.locations.get(0);
         }
 
-       return this.locations.get(0);
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for(int i = 0; i<locations.size(); i++){
+            builder.include(locations.get(i));
+        }
+
+       return builder.build().getCenter();
     }
 
+    /**
+     * A getter for the clusters locations
+     * @return The clusters associated locations
+     */
     public ArrayList<LatLng> getLocations(){
         return this.locations;
     }
 
+    /**
+     * A utility method to add a group of new locations to the cluster
+     * @param new_locations The new locations to merge into the cluster
+     */
     public void addLocations(ArrayList<LatLng> new_locations){
         this.locations.addAll(new_locations);
     }
