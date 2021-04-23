8aa16d72f927e0919961ba078dd569b3a42871ee
bfhorswell4 <bfhorswell@gmail.com>
2021-04-23 15:58:37 +0100

    adding a location now starts up an autocomplete intent that interfaces with the Google Places API. The location result is recieved in onActivityResult and printed

--- app/src/main/java/com/example/clust/MapsActivity.java
+++ app/src/main/java/com/example/clust/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.clust;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,6 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -24,6 +27,7 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static int AUTOCOMPLETE_REQUEST_CODE = 1;
     private GoogleMap mMap;
     FloatingActionButton addLocationButton;
 
@@ -43,7 +47,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Testing Add Location!", Toast.LENGTH_LONG).show();
+                // Set fields for what type of place data should be returned on user selection
+                List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS,
+                        Place.Field.LAT_LNG, Place.Field.NAME);
+
+                // Starts up an autocomplete intent
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,
+                        fieldList).build(MapsActivity.this);
+                startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
             }
         });
     }
@@ -65,5 +76,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Result succeeded
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Toast.makeText(getApplicationContext(), "Place: " + place.getName() + ", " + place.getId(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // Handle the error
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Toast.makeText(getApplicationContext(), status.getStatusMessage(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
